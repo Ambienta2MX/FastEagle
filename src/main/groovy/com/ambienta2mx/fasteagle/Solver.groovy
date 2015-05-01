f268e694ecc3c2858ac9798e357cbea5513df609
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-05-01 17:32:08 -0500

    Working on #1, updating dependencies
    Adding HTTP Builder, GPars and MySQL Connector jars

--- src/main/groovy/com/ambienta2mx/fasteagle/Solver.groovy
+++ src/main/groovy/com/ambienta2mx/fasteagle/Solver.groovy
@@ -1,7 +1,19 @@
 package com.ambienta2mx.fasteagle
 
+import groovyx.net.http.HTTPBuilder
+
 /**
  * Created by alberto on 4/16/15.
  */
 class Solver {
+    def http
+    def findSuitablePlace(def place) {
+        http = new HTTPBuilder('https://maps.googleapis.com/maps/api/geocode/');
+        def latitude = place.latitud/10000 +
+
+    }
+
+    def getGeographicCoordinates(def place) {
+        http  = new HTTPBuilder('https://maps.googleapis.com/maps/api/geocode/')
+    }
 }
--- src/test/groovy/SolverSpec.groovy
+++ src/test/groovy/SolverSpec.groovy
@@ -9,7 +9,15 @@ class SolverSpec extends spock.lang.Specification {
     def "Should solve a location"() {
         expect:
         solver.findSuitablePlace(place) == result
-        result:
+        where:
+        place                                                                                                                       || result
+        ['LATITUD': '191658', 'LONGITUD': '0991716', 'valid': true, 'solve': true] || ['LATITUD': 0, 'LONGITUD': 0, 'NOMBRE_EDO': 'Distrito Federal', 'NOMBRE_MUN': 'Azcapotzalco', 'valid': true, 'solve': true]
+    }
+
+    def "Should get itrf/nad27 coordinates"() {
+        expect:
+        solver.getGeographicCoordinates(place) == result
+        where:
         place                                                                                                                       || result
         ['LATITUD': 0, 'LONGITUD': 0, 'NOMBRE_EDO': 'Distrito Federal', 'NOMBRE_MUN': 'Azcapotzalco', 'valid': true, 'solve': true] || ['LATITUD': 0, 'LONGITUD': 0, 'NOMBRE_EDO': 'Distrito Federal', 'NOMBRE_MUN': 'Azcapotzalco', 'valid': true, 'solve': true]
     }
