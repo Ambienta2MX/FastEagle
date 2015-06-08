6a931ed912273d6203af1086eaa50369f2fd45b1
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-06-08 00:20:46 -0500

    Coding #1, Updating model
    Updating model and adding properties file

---
+++ src/main/groovy/com/ambienta2mx/fasteagle/FastEagle.groovy
@@ -0,0 +1,13 @@
+package com.ambienta2mx.fasteagle
+
+import groovy.util.logging.Log
+
+/**
+ * Created by alberto on 6/7/15.
+ */
+@Log
+class FastEagle {
+  def main(def args){
+    log.error("OK")
+  }
+}
--- src/main/groovy/com/ambienta2mx/fasteagle/Place.groovy
+++ src/main/groovy/com/ambienta2mx/fasteagle/Place.groovy
@@ -4,4 +4,18 @@ package com.ambienta2mx.fasteagle
  * Created by alberto on 4/16/15.
  */
 class Place {
+    def location //GEOJson Spec
+    List sexagesimal_coordinates
+    List itrf_coordinates
+    List nad27_coordinates
+    def height
+    String town
+    String state
+    String city
+    String fullName
+    String zipCode
+    List extraInfo
+    List provider
+    Date lastUpdated
+    Date dateCreated
 }
