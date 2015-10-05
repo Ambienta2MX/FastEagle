4f448d84a683ca847054a0b9df592cded14cab7c
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-05 01:08:13 -0500

    #3, Updating google service test, now it looks for information through web api
    Using Google Geocoding API now we look for places and then fill the standar model from Ambienta2MX via lat/lng

--- src/test/groovy/LocationServiceSpec.groovy
+++ src/test/groovy/LocationServiceSpec.groovy
@@ -4,22 +4,30 @@ import mx.ipn.ambienta2mx.fastEagle.services.LocationService
  * Created by alberto on 4/25/15.
  */
 class LocationServiceSpec extends spock.lang.Specification {
-    LocationService solver = new LocationService()
 
     def "Should solve a location via Lat/Lon"() {
+        given:
+        LocationService solver = new LocationService("AIzaSyCo72iVxPewCLtXKmoeiWSyNAAVTqIiVvs")
         expect:
-        solver.solvePlaceByLatLon(latitude, longitude) == result
+        Map solverResponse = solver.solvePlaceByLatLon(latitude, longitude)
+        solverResponse.fullName == result.fullName
         where:
-        latitude   | longitude || result
-        -99.186510 | 19.504434 || [fullName: 'Distrito Federal, Azcapotzalco, San Pablo Xalpa']
+        longitude  | latitude   || result
+        -99.186510 | 19.504434  || [fullName: 'Distrito Federal, Azcapotzalco, San Pablo Xalpa']
+        -99.019232 | 19.3740416 || [fullName: 'Distrito Federal, Iztapalapa, Zona Urbana Ejidal Santa Martha Acatitla Sur']
+        -25.019232 | 88.3740416 || [fullName: null]
     }
 
     def "Should solve a location via Place"() {
+        given:
+        LocationService solver = new LocationService("AIzaSyCo72iVxPewCLtXKmoeiWSyNAAVTqIiVvs")
         expect:
-        solver.solvePlaceByName(place) == result
+        solver.solvePlaceByName(place).itrf_coordinates == result.itrf_coordinates
         where:
         place                                                           || result
-        ['fullName': 'Distrito Federal, Azcapotzalco, San Pablo Xalpa'] || ['coordinates': [-99.186510, 19.504434]]
-        ['fullName': 'Distrito Federal, Gustavo A. Madero, Lindavista'] || ['coordinates': [-99.146569, 19.504664]]
+        ['fullName': 'Distrito Federal, Azcapotzalco, San Pablo Xalpa'] || ['itrf_coordinates': [-99.186510, 19.504434]]
+        ['fullName': 'Distrito Federal, Gustavo A. Madero, Lindavista'] || ['itrf_coordinates': [-99.146569, 19.504664]]
+        ['fullName': 'Ahuehuetes La perla Nezahualcóyotl']              || ['itrf_coordinates': [-98.9926226, 19.3866537]]
+        ['fullName': 'lugar que no existe']                             || ['itrf_coordinates': null]
     }
 }
