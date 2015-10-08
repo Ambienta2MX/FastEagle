91596251ae5499d8f80ad3b67408909b0acd758b
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-08 08:28:48 -0500

    #3, Updating LocationService and Gradle Sources

--- src/main/groovy/mx/ipn/ambienta2mx/fastEagle/services/LocationService.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/fastEagle/services/LocationService.groovy
@@ -18,17 +18,15 @@ class LocationService {
     Map solvePlaceByLatLon(def longitude, def latitude) {
         Place place = new Place()
         http = new HTTPBuilder('https://maps.googleapis.com/maps/api/geocode/');
-        Map jsonResponse = [:]
         http.get(path: "json", query: [latlng: "$latitude,$longitude", key: "$currentKey", "language": "es"]) { resp, json ->
-            jsonResponse = json
             // Avoiding a null response
-            if (jsonResponse.results) {
+            if (json.results) {
                 // Parsing the Json information from google
-                def location = jsonResponse.results[0].geometry.location
+                def location = json.results[0].geometry.location
                 List coordinates = [location.lng, location.lat]
-                def addressComponents = jsonResponse.results[0].address_components
+                def addressComponents = json.results[0].address_components
                 // Retrieving just information from Mexico
-                if (jsonResponse.results[-1].address_components[0].short_name == "MX") {
+                if (json.results[-1].address_components[0].short_name == "MX") {
                     place.itrf_coordinates = coordinates
                     place.nad27_coordinates = coordinates
                     place.location = [coordinates: coordinates, type: "Point"]
@@ -55,7 +53,26 @@ class LocationService {
         http = new HTTPBuilder('https://maps.googleapis.com/maps/api/geocode/');
         Place place = new Place()
         http.get(path: "json", query: [address: "$address", key: "$currentKey"]) { resp, json ->
+            // Avoiding a null response
+            if (json.results) {
+                // Parsing the Json information from google
+                def location = json.results[0].geometry.location
+                List coordinates = [location.lng, location.lat]
+                def addressComponents = json.results[0].address_components
+                // Retrieving just information from Mexico
+                if (json.results[-1].address_components[0].short_name == "MX") {
+                    place.itrf_coordinates = coordinates
+                    place.nad27_coordinates = coordinates
+                    place.location = [coordinates: [coordinates], type: "Point"]
+                    place.sexagesimal_coordinates = [] // Just INEGI Sources contains this information
 
+                    place.zipCode = addressComponents.find { element -> "postal_code" in element.types }.long_name ?: ""
+                    place.state = addressComponents.find { element -> "administrative_area_level_1" in element.types }.long_name ?: ""
+                    place.city = addressComponents.find { element -> "administrative_area_level_3" in element.types }.long_name ?: ""
+                    place.town = addressComponents.find { element -> "sublocality" in element.types }.long_name ?: ""
+                    place.fullName = "$place.state, $place.city, $place.town"
+                }
+            }
         }
         return place.properties
     }
