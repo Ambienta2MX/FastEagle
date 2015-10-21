e736ee329a37ab33a614a261345ea66261042b38
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-20 23:56:37 -0500

    #4, Changing response type,it always sends an array as reponse, Fixes #4

--- src/main/groovy/mx/ipn/ambienta2mx/fastEagle/web/RouteClosures/RoutesDefinition.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/fastEagle/web/RouteClosures/RoutesDefinition.groovy
@@ -1,7 +1,6 @@
 package mx.ipn.ambienta2mx.fastEagle.web.RouteClosures
 
 import groovy.json.JsonOutput
-import org.apache.commons.collections.Closure
 
 /**
  * Created by alberto on 11/10/15.
@@ -12,7 +11,7 @@ class RoutesDefinition {
     def eventBus
 
     def byLatLon = { request ->
-        def coordinates = [Double.parseDouble(request.params.latitude ?: 0), Double.parseDouble(request.params.longitude ?: 0)]
+        def coordinates = [Double.parseDouble(request.params.longitude ?: 0), Double.parseDouble(request.params.latitude ?: 0)]
         def maxDistance = Double.parseDouble(request.params.maxDistance ?: 100)
         def query = [
                 action : 'find', collection: 'Places',
@@ -31,7 +30,8 @@ class RoutesDefinition {
                 request.response.end "${JsonOutput.toJson(mongoResponse.body.results)}"
             } else { // No results, trying to resolve the information via google maps
                 eventBus.send("$definedConfiguration.location.address", [method: 'latlng', coordinates: coordinates]) { message ->
-                    request.response.end "${JsonOutput.toJson(message.body)}"
+                    def documents = [message.body]
+                    request.response.end "${JsonOutput.toJson(documents)}"
                 }
             }
 
@@ -55,7 +55,8 @@ class RoutesDefinition {
                 request.response.end "${JsonOutput.toJson(mongoResponse.body.results)}"
             } else { // No results, trying to resolve the information via google maps
                 eventBus.send("$definedConfiguration.location.address", [method: 'name', name: name]) { message ->
-                    request.response.end "${JsonOutput.toJson(message.body)}"
+                    def documents = [message.body]
+                    request.response.end "${JsonOutput.toJson(documents)}"
                 }
             }
         }
